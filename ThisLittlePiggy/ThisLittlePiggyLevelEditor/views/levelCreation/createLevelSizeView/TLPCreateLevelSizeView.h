4efb5ccd980509a836c8252f666fd5a06298cdb0
Jabari Bell <jabari.bell@pxlflu.net>
2013-11-10 16:35:57 -0500

    moved panel around, adding views to handle different creation states

---
+++ ThisLittlePiggy/ThisLittlePiggyLevelEditor/views/levelCreation/createLevelSizeView/TLPCreateLevelSizeView.h
@@ -0,0 +1,13 @@
+//
+//  TLPCreateLevelSizeView.h
+//  ThisLittlePiggy
+//
+//  Created by Jabari on 11/10/13.
+//  Copyright (c) 2013 23bit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TLPCreateLevelSizeView : UIView
+
+@end
---
+++ ThisLittlePiggy/ThisLittlePiggyLevelEditor/views/levelCreation/createLevelSizeView/TLPCreateLevelSizeView.m
@@ -0,0 +1,31 @@
+//
+//  TLPCreateLevelSizeView.m
+//  ThisLittlePiggy
+//
+//  Created by Jabari on 11/10/13.
+//  Copyright (c) 2013 23bit. All rights reserved.
+//
+
+#import "TLPCreateLevelSizeView.h"
+
+@implementation TLPCreateLevelSizeView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
