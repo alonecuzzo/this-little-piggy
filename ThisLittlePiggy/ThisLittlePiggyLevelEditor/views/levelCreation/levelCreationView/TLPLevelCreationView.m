13245058bbe7591bda131e96677cbb3f0ad97f9f
Jabari Bell <jabari.bell@pxlflu.net>
2013-11-11 16:14:20 -0500

    menu stubbed out for the levelcreator, using gestures, too much time to figure out how to work with reactivecocoa

--- ThisLittlePiggy/ThisLittlePiggyLevelEditor/views/levelCreation/levelCreationView/TLPLevelCreationView.m
+++ ThisLittlePiggy/ThisLittlePiggyLevelEditor/views/levelCreation/levelCreationView/TLPLevelCreationView.m
@@ -9,18 +9,24 @@
 #import "TLPLevelCreationView.h"
 #import "UIColor+ThisLittlePiggy.h"
 #import "TLPLevelCreationGridView.h"
+#import "TLPLevelCreationMenuView.h"
 
 @implementation TLPLevelCreationView
 {
     UIScrollView *_scrollView;
     UIView *_levelView;
     TLPLevelCreationGridView *_gridView;
+    TLPLevelCreationMenuView *_menu;
 }
 
 - (id)initWithFrame:(CGRect)frame andLevelSize:(CGSize)levelSize
 {
     self = [super initWithFrame:frame];
     if (self) {
+        //menu
+        _menu = [[TLPLevelCreationMenuView alloc] initWithFrame:frame];
+        [self addSubview:_menu];
+
         _scrollView = [[UIScrollView alloc] initWithFrame:frame];
         _scrollView.scrollEnabled = YES;
         _scrollView.userInteractionEnabled = YES;
@@ -31,13 +37,25 @@
         
         _levelView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, levelSize.width, levelSize.height)];
         _levelView.backgroundColor = [UIColor thisLittlePiggyPaleBlue];
+        _levelView.userInteractionEnabled = YES;
         [_scrollView setContentSize:levelSize];
         [_scrollView addSubview:_levelView];
         
         _gridView = [[TLPLevelCreationGridView alloc] initWithFrame:_levelView.frame];
         [_levelView addSubview:_gridView];
+
+        UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(toggleMenu)];
+        tapGestureRecognizer.numberOfTapsRequired = 2;
+        [_levelView addGestureRecognizer:tapGestureRecognizer];
+
+        [_menu setupViews];
     }
     return self;
+}
+
+- (void)toggleMenu
+{
+    [_menu toggle];
 }
 
 @end
---
+++ ThisLittlePiggy/ThisLittlePiggyLevelEditor/views/levelCreation/levelCreationView/menu/TLPLevelCreationMenuView.m
@@ -0,0 +1,89 @@
+//
+//  TLPLevelCreationMenuView.m
+//  ThisLittlePiggy
+//
+//  Created by Jabari on 11/11/13.
+//  Copyright (c) 2013 23bit. All rights reserved.
+//
+
+#import "TLPLevelCreationMenuView.h"
+#import "UIView+GCLibrary.h"
+
+@implementation TLPLevelCreationMenuView
+{
+    BOOL _isOpen;
+    UIView *_minimap;
+    UIView *_sideMenu;
+}
+
+#pragma mark - init stuff
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        //stub out the minimap and side menu
+        CGSize minimapSize = CGSizeMake(200.0f, 100.0f);
+        CGRect minimapFrame = CGRectMake(self.frame.size.width + 10.0f, self.frame.size.height - minimapSize.height - 10.0f, minimapSize.width, minimapSize.height);
+        _minimap = [[UIView alloc] initWithFrame:minimapFrame];
+        _minimap.backgroundColor = [UIColor purpleColor];
+        _minimap.alpha = 0.8f;
+
+        CGSize sideMenuSize = CGSizeMake(150.0f, self.frame.size.height);
+        CGRect sideMenuRect = CGRectMake(-sideMenuSize.width - 5.0f, 0.0f, sideMenuSize.width, sideMenuSize.height);
+        _sideMenu = [[UIView alloc] initWithFrame:sideMenuRect];
+        _sideMenu.backgroundColor = [UIColor redColor];
+        _sideMenu.alpha = 0.8;
+    }
+    return self;
+}
+
+- (void)setupViews
+{
+    [self.superview addSubview:_minimap];
+    [self.superview addSubview:_sideMenu];
+}
+
+- (void)toggle
+{
+    if (_isOpen) {
+        [self close];
+        _isOpen = NO;
+    } else {
+        [self open];
+        _isOpen = YES;
+    }
+}
+
+- (void)open
+{
+    id __weak weakSelf = self;
+    [UIView animateWithDuration:0.4 animations:^{
+       TLPLevelCreationMenuView *strongSelf = (TLPLevelCreationMenuView *)weakSelf;
+       CGRect r = CGRectMake(strongSelf.frame.size.width - 10 - _minimap.frame.size.width, _minimap.frame.origin.y, _minimap.frame.size.width, _minimap.frame.size.height);
+        [_minimap setFrame:r];
+
+        r = CGRectMake(5.0f, 0.0f, _sideMenu.frame.size.width, strongSelf.height);
+        [_sideMenu setFrame:r];
+    } completion:^(BOOL completed) {
+        
+    }];
+}
+
+- (void)close
+{
+    id __weak weakSelf = self;
+    [UIView animateWithDuration:0.4 animations:^{
+        TLPLevelCreationMenuView *strongSelf = (TLPLevelCreationMenuView *)weakSelf;
+        CGRect r = CGRectMake(strongSelf.frame.size.width + 10, _minimap.frame.origin.y, _minimap.frame.size.width, _minimap.frame.size.height);
+        [_minimap setFrame:r];
+
+        r = CGRectMake(-_sideMenu.frame.size.width - 5.0f, 0.0f, _sideMenu.frame.size.width, strongSelf.height);
+        [_sideMenu setFrame:r];
+    } completion:^(BOOL completed) {
+
+    }];
+}
+
+
+@end
